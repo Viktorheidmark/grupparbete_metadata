cf1afabca0e2eac934940c84a894f25140754ddc
Benereta Hox <beneretahoxha8@gmail.com>
2025-08-29 14:55:21 +0200

    commit

--- pdf-rest-routes.js
+++ pdf-rest-routes.js
@@ -2,21 +2,23 @@ export default function setupPdfRestRoutes(app, db) {
 
     app.get('/api/pdf-search/:field/:searchValue', async (req, res) => {
         // get field and searhValue from the request parameters
-        const { field, searchValue } = req.params;
+        let { field, searchValue } = req.params;
         // check that field is a valid field, if not do nothing
         if (!['title', 'author'].includes(field)) {
             res.json({ error: 'Invalid field name!' });
             return;
         }
+        // capitalize field name
+        field = field[0].toUpperCase() + field.slice(1);
+
         // run the db query as a prepared statement
         const [result] = await db.execute(`
-    SELECT id,meta->>'$.fileName' AS fileName,
-      meta->>'$.common.title' AS titel,
-      meta->>'$.common.author' AS författare,
-    FROM pdf
-    WHERE LOWER(meta->>'$.common.${field}') LIKE LOWER(?)
-  `, ['%' + searchValue + '%']
-        );
+            SELECT id,meta->>'$.fileName' AS fileName,
+            meta->>'$.info.Title' AS title,
+            meta->>'$.info.Author' AS author
+            FROM pdf
+            WHERE LOWER(meta->>'$.info.${field}') LIKE LOWER(?)
+        `, ['%' + searchValue + '%']);
         // return the result as json
         res.json(result);
     });
@@ -25,8 +27,8 @@ export default function setupPdfRestRoutes(app, db) {
     app.get('/api/pdf-all-meta/:id', async (req, res) => {
         const { id } = req.params;
         let [result] = await db.execute(`
-    SELECT * FROM pdf WHERE id = ?
-  `, [id]);
+            SELECT * FROM pdf WHERE id = ?
+        `, [id]);
         res.json(result);
     });
 
