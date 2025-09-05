f15bd6aa707176fd6f54dc7b94ee6988fe3c47af
Benereta Hox <beneretahoxha8@gmail.com>
2025-09-05 12:36:24 +0200

    startpage skapat

---
+++ frontend/startpage.js
@@ -0,0 +1,8 @@
+export function startPageContent() {
+    return `
+    <h1>Start</h1>
+    <p>Välkommen till våra sökmotor för metadata, där det ska kunna söka i viktiga filer från företagets filservrar.</p>
+    <p>Vi är Tannaz, Isse, Viktor och Benereta som har skapat detta sida.</p>
+    <p>Kontakta oss för fler frogår. <a href="mailto:thomas@nodehill.com">thomas@nodehill.com</a>.</p>
+  `;
+}
